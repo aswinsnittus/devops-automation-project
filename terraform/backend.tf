5c5ad7df585c764ad24a27f61484fdba1b232377
Aswin Snittu S <aswinsnittu@karunya.edu.in>
2025-10-31 11:34:30 +0530

    Configure S3 backend for Terraform state

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+﻿terraform {
+  backend "s3" {
+    bucket         = "devops-tfstate-aswinsnittu-20251031112102"
+    key            = "devops-project/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "devops-tf-locks-aswinsnittu-20251031112102"
+    encrypt        = true
+  }
+}
